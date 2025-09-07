6963aabfc8b5b51e3481d60a916b8c6bb2ea3d3c
Edulabs-fr <edulabs.svc@gmail.com>
2025-09-07 15:31:40 +0200

    ERG

--- Microsoft/Azure/InfraLabs/deploiement.sh
+++ Microsoft/Azure/InfraLabs/deploiement.sh
@@ -27,7 +27,20 @@ vnet_spk_p_b_ip="10.2.0.0/16"
 subnet_spoke_prod_back_dbwordpress_name="sub_spk_p_b_dbwp"
 sub_spk_p_b_dbwp_ip="10.2.1.0/24"
 
+name_nic_prod_front_wp_client1="wp_front_client1"
+name_nic_prod_back_dbwp_client1="dbwp_back_client1"
 
+name_vault_prod="edulabsVault"
+
+name_ip_pub_bastion="ip-pub-bastion"
+name_ip_pub_lb_prod_front="ip-pub-lb-prod-front"
+name_ip_pub_vm_wp_front_temp="ip-pub-vm-temp"
+
+name_vm_prod_front_wp_client1="wp-client01"
+name_vm_prod_back_dbwp_client1="dbwp-client01"
+
+name_nsg_prod_front="nsg-prod-front"
+name_nsg_prod_back="nsg-prod-back"
 
 #  Vnet   #
 #----------
@@ -49,7 +62,6 @@ az network vnet create -g $rg -l $location --name $vnet_spoke_prod_back_name --a
 id_vnet_spoke_prod_back=$(az network vnet show --name $vnet_spoke_prod_back_name -g $rg --query id -o tsv)
 
 
-
 #  subnet   #
 #------------
 
@@ -57,8 +69,6 @@ id_vnet_spoke_prod_back=$(az network vnet show --name $vnet_spoke_prod_back_name
 az network vnet subnet create -g $rg --vnet-name $vnet_spoke_prod_front_name -n $subnet_spoke_prod_front_wordpress_name --address-prefixes $sub_spk_p_f_wp_ip
 #db-wordpress
 az network vnet subnet create -g $rg --vnet-name $vnet_spoke_prod_back_name -n $subnet_spoke_prod_back_dbwordpress_name --address-prefixes $sub_spk_p_b_dbwp_ip
-
-
 
 
 # Création Peering :
@@ -72,12 +82,6 @@ az network vnet peering create -g $rg -n SpokeProdFrontToHub --vnet-name $vnet_s
 #Prod_Back_Spoke_To_Hub
 az network vnet peering create -g $rg -n SpokeProdBackToHub --vnet-name $vnet_spoke_prod_back_name --remote-vnet $vnet_hub_name --allow-vnet-access
 
-
-
-
-name_vault_prod="edulabsVault"
-
-
 #Azure Keyvault
 az keyvault create --name $name_vault_prod --resource-group $rg --location $location
 
@@ -85,16 +89,9 @@ az keyvault create --name $name_vault_prod --resource-group $rg --location $loca
 az keyvault secret set --vault-name $name_vault_prod --name "AdminPassword" --value "Motdepasse123!"
 
 
-
-
 ########################################
 #VM
 ########################################
-
-
-name_ip_pub_bastion="ip-pub-bastion"
-name_ip_pub_lb_prod_front="ip-pub-lb-prod-front"
-name_ip_pub_vm_wp_front_temp="ip-pub-vm-temp"
 
 # Public_IP's #
 #--------------
@@ -108,38 +105,29 @@ az network public-ip create -g $rg -l $location -n $name_ip_pub_lb_prod_front
 az network public-ip create -g $rg -l $location -n $name_ip_pub_vm_wp_front_temp
 
 
-
-
-name_nic_prod_front_wp_client1="wp_front_client1"
-name_nic_prod_front_dbwp_client1="dbwp_back_client1"
-
-
 # Nic's #
 #--------
 #NIC VM Wordpress Client 1
-az network nic create -g $rg -l $location -n $name_nic_prod_front_wp_client1 --vnet-name spoke-prod-front --subnet wordpress --public-ip-address ip-pub-wp-clt1
+az network nic create -g $rg -l $location -n $name_nic_prod_front_wp_client1 --vnet-name $vnet_spoke_prod_front_name --subnet $subnet_spoke_prod_front_wordpress_name --public-ip-address $name_ip_pub_vm_wp_front_temp
 
 #NIC VM DB-Wordpress Client 1
-az network nic create -g $rg -l $location -n $name_nic_prod_front_dbwp_client1 --vnet-name spoke-prod-back --subnet db-wordpress
+az network nic create -g $rg -l $location -n $name_nic_prod_back_dbwp_client1 --vnet-name $vnet_spoke_prod_back_name --subnet $subnet_spoke_prod_back_dbwordpress_name
 
 
-
-vm-prod-front-client1="vm-prod-front-client1"
 
 # VM's #
 #-------
 
 #VM Wordpress Front
-
 az vm create \
   --resource-group $rg \
   --location $location \
-  --name vm-prod-front-client1 \
+  --name $name_vm_prod_front_wp_client1 \
   --image Debian:debian-11:11:latest \
   --size Standard_B1ms \
   --admin-username "lotfi" \
   --admin-password "Motdepasse123!" \
-  --nics "nic-vm-prod-front-wp-clt1" \
+  --nics $name_nic_prod_front_wp_client1 \
   --os-disk-delete-option Delete \
   --storage-sku Premium_LRS \
   --zone 1
@@ -149,12 +137,12 @@ az vm create \
 az vm create \
   --resource-group $rg \
   --location $location \
-  --name vm-prod-back-client1 \
+  --name $name_vm_prod_back_dbwp_client1 \
   --image Debian:debian-11:11:latest \
   --size Standard_B1ms \
   --admin-username "lotfi" \
   --admin-password "Motdepasse123!" \
-  --nics "nic-vm-prod-back-wp-clt1" \
+  --nics $name_nic_prod_back_dbwp_client1 \
   --os-disk-delete-option Delete \
   --storage-sku Premium_LRS \
   --zone 1
@@ -166,73 +154,50 @@ az vm create \
 
 
 
+
 ########################################
 #   NSG & ASG
 ########################################
 
-
-#ASG Front Wordpress Client1
-az network asg create \
-  --name asg1-wordpress-client1 \
-  --resource-group $rg\
-  --location $location
-
-nic_wp_prod_ctl1_conf=$(az network nic show --name nic-vm-prod-front-wp-clt1 --resource-group $rg --query "ipConfigurations[].name" --output tsv)
-
-az network nic ip-config update \
-  --name $nic_wp_prod_ctl1_conf \
-  --nic-name nic-vm-prod-front-wp-clt1 \
-  --resource-group $rg \
-  --application-security-groups asg1-wordpress-client1
-
-
-#ASG Back Wordpress Client1
-az network asg create \
-  --name asg1-db-wp-client1 \
-  --resource-group $rg\
-  --location $location
-
-nic_wp_back_ctl1_conf=$(az network nic show --name nic-vm-prod-back-wp-clt1 --resource-group $rg --query "ipConfigurations[].name" --output tsv)
-
-az network nic ip-config update \
-  --name $nic_wp_back_ctl1_conf \
-  --nic-name nic-vm-prod-back-wp-clt1 \
-  --resource-group $rg \
-  --application-security-groups asg1-db-wp-client1
-
-
-
 ##################### NSG Front Web
 az network nsg create \
-  --name nsg-front-prod \
+  --name $name_nsg_prod_front \
   --resource-group $rg \
   --location $location
 
-# NSG Rule Back Wordpress
+az network nic update \
+  --resource-group $rg \
+  --name $name_nic_prod_front_wp_client1 \
+  --network-security-group $name_nsg_prod_front
 
+
+# NSG Rule Back Wordpress
 az network nsg rule create \
   --resource-group $rg \
-  --nsg-name nsg-front-prod \
-  --name AllowSSH \
-  --priority 100 \
+  --nsg-name $name_nsg_prod_front \
+  --name AllowSSHToSubnet \
+  --priority 150 \
   --direction Inbound \
   --access Allow \
   --protocol Tcp \
   --source-address-prefixes '*' \
-  --destination-asgs asg1-wordpress-client1 \
+  --destination-address-prefixes $name_ip_pub_vm_wp_front_temp \
   --destination-port-ranges 22
 
 az network nsg rule create \
   --resource-group $rg \
-  --nsg-name nsg-front-prod \
-  --name AllowHTTP \
-  --priority 110 \
+  --nsg-name $name_nsg_prod_front \
+  --name AllowSSHToSubnet \
+  --priority 155 \
   --direction Inbound \
   --access Allow \
   --protocol Tcp \
   --source-address-prefixes '*' \
-  --destination-asgs asg1-wordpress-client1 \
+  --destination-address-prefixes $name_ip_pub_vm_wp_front_temp \
   --destination-port-ranges 80
+
+
+
 
 
 ##################### NSG Front Web
